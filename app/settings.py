b4b270cdf57a5ec6c03aa25fc3b54067a6da5feb
mhamed <mohamedhame2ed@gmail.com>
2023-02-21 04:33:42 +0200

    add new deploy

--- app/settings.py
+++ app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG' , cast=bool)
-ALLOWED_HOST = ['*']
+DEBUG = config('DEBUG', cast=bool)
 
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
