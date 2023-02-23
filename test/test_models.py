20114493ad71ac6f62a854ffa3822df0084f052f
mhamed <mohamedhame2ed@gmail.com>
2023-02-23 04:05:22 +0200

    add test app

--- test/test_models.py
+++ test/test_models.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
 from ProjectApp.models import Product
 from django.contrib.auth.models import User
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
+
 
 
 class ProductModelTest(TestCase):
@@ -24,10 +22,7 @@ class ProductModelTest(TestCase):
         expected_object_name = f'{author.name}'
         self.assertEqual(str(author), expected_object_name)
 
-    # def test_valid_form(self):
-    #     user = User.objects.create_user(id=2, username='hamednour', password='password')
-    #     validData = Product.objects.create(admin=user, name='zara-shirt', price=250.0)
-    #     self.assertTrue(validData)
+
 
 
 
